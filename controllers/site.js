32d7abdb6114a439a01166d86445d9177c38dfa3
Elaina Tia <elaina.tia@LMRC5350.local>
2016-05-26 21:29:33 -0700

    add global css files

--- controllers/site.js
+++ controllers/site.js
@@ -6,31 +6,31 @@ var site_routes = function(app) {
 	});
 
 	app.get('/about', function(req, res) {
-		res.render('about');
+		res.render('about', {css: 'css/about.css'});
 	});
 
 	app.get('/resume', function(req, res) {
-		res.render('resume');
+		res.render('resume', {css: 'css/resume.css'});
 	});
 
 	app.get('/fun', function(req, res) {
-		res.render('fun');
+		res.render('fun', {css: 'css/fun.css'});
 	});
 
 	app.get('/contact', function(req, res) {
-		res.render('contact');
+		res.render('contact', {css: 'css/contact.css'});
 	});
 
 	app.get('/express-project', function(req, res) {
-		res.render('express-project');
+		res.render('express-project', {css: 'css/express-project.css'});
 	});
 
 	app.get('/glip-project', function(req, res) {
-		res.render('glip-project');
+		res.render('glip-project', {css: 'css/glip-project.css'});
 	});
 
 	app.get('/iwatch-project', function(req, res) {
-		res.render('iwatch-project');
+		res.render('iwatch-project', {css: 'css/iwatch-project.css'});
 	});	
 };
 
