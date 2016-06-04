e480c955fd32b103d07959327b77d40554a0c89b
Derek Tia <dtia05@gmail.com>
2016-06-04 10:16:56 -0700

    add nav bar highlighting

--- controllers/site.js
+++ controllers/site.js
@@ -2,12 +2,12 @@ var _ = require('underscore');
 
 var site_routes = function(app) {
 	app.get('/', function (req, res) {
-		res.render('home', {css: 'css/home.css'});
+		res.render('home', {css: 'css/home.css', page: 'home'});
 	});
 
 	app.get('/:pageName', function (req, res) {
 		var page = req.params.pageName;
-	  	res.render(page, {css: 'css/' + page + '.css'});
+	  	res.render(page, {css: 'css/' + page + '.css', page: page});
 	});
 };
 
