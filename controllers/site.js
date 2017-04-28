9446312b5c831d9c877566689665ad9039367c89
Derek Tia <dtia05@gmail.com>
2017-04-27 23:30:35 -0700

    more updates

--- controllers/site.js
+++ controllers/site.js
@@ -2,7 +2,7 @@ var _ = require('underscore');
 
 var site_routes = function(app) {
 	app.get('/', function (req, res) {
-		res.render('home', {css: 'css/home.css', page: 'home'});
+		res.render('projects', {page: 'projects'});
 	});
 
 	app.get('/:pageName', function (req, res) {
